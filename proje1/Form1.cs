3f41ba86ae763c43c3e2ea53daaf5927102ed2ea
zehranurozturk02 <zehranurozturk783@gmail.com>
2024-04-03 23:00:55 +0300

    Proje dosyası ekle.

---
+++ proje1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje1
+     
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+             private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="zehra" && textBox2.Text == "123")
+            {
+                Form2 yeni = new Form2();
+                yeni.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("giriş hatalı tekrar deneyiniz");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+        }
+    }
+}
---
+++ proje1/Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if(comboBox1.Text== "mavi")
+            {
+                label4.Text = textBox1.Text + " isimli kişi mavi rengini seçti";
+            }else if (comboBox1.Text == "sarı")
+            {
+                label4.Text = textBox1.Text + " isimli kişi sarı rengini seçti" ;
+            }else if (comboBox1.Text == "kırmızı")
+            {
+                label4.Text = textBox1.Text + " isimli kişi kırmızı rengini seçti";
+            }
+            else
+            {
+                MessageBox.Show("listedeki rengi seçmediniz");
+            }
+            }
+        }
+    }
+
